fdaeff73f65acb0746c92d3b882864d12863e404
mentalNika <mandrygina798@mail.ru>
2025-07-10 02:30:07 +0300

    fix: metadata

--- src/app/portfolio/[id]/page.tsx
+++ src/app/portfolio/[id]/page.tsx
@@ -2,38 +2,38 @@ import { GalleryPage } from "@/components/pages/GalleryPage/GalleryPage";
 import { portfolioData } from "@/components/pages/PortfolioPage/PortfolioPage";
 import type { Metadata } from "next";
 
-type PageParams = {
-  params: { id: string };
-};
+// type PageParams = {
+//   params: { id: string };
+// };
 
-export async function generateMetadata({
-  params,
-}: PageParams): Promise<Metadata> {
-  const project = portfolioData.find((item) => item.id === Number(params.id));
+// export async function generateMetadata({
+//   params,
+// }: PageParams): Promise<Metadata> {
+//   const project = portfolioData.find((item) => item.id === Number(params.id));
 
-  if (!project) {
-    return {
-      title: "Проект не найден",
-    };
-  }
+//   if (!project) {
+//     return {
+//       title: "Проект не найден",
+//     };
+//   }
 
-  return {
-    title: `VibeVis | ${project.title}`,
-    description: `Проект: ${project.title} — визуализация интерьера`,
-    openGraph: {
-      title: `VibeVis | ${project.title}`,
-      description: `Проект: ${project.title} — визуализация интерьера`,
-      images: [
-        {
-          url: project.images[0],
-          width: 1200,
-          height: 630,
-          alt: project.title,
-        },
-      ],
-    },
-  };
-}
+//   return {
+//     title: `VibeVis | ${project.title}`,
+//     description: `Проект: ${project.title} — визуализация интерьера`,
+//     openGraph: {
+//       title: `VibeVis | ${project.title}`,
+//       description: `Проект: ${project.title} — визуализация интерьера`,
+//       images: [
+//         {
+//           url: project.images[0],
+//           width: 1200,
+//           height: 630,
+//           alt: project.title,
+//         },
+//       ],
+//     },
+//   };
+// }
 
 export default function page() {
   return <GalleryPage />;
